bd8b627777a21ad95023ced2792aa0762e1dbd07
jamesramsayboyd <jamesramsayboyd@gmail.com>
2024-10-22 12:59:17 +0800

    Starting to work on computer opponent logic

--- main.lua
+++ main.lua
@@ -3,6 +3,9 @@
 -- main.lua
 --
 -----------------------------------------------------------------------------------------
+require("lunatest") --import the test framework
+require("game")
+require("tests.mytests") -- import the tests and run them
 
 local composer = require( "composer" )
 
